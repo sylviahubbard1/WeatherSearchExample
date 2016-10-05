de6ea4d67be08d6352186021715394133b0caa0f
sylviahubbard1 <sylviahubbard@gmail.com>
2016-10-05 10:42:40 -0400

    Add project files.

---
+++ GooglePlacesExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GooglePlacesExample.Startup))]
+namespace GooglePlacesExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
